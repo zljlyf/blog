0dd2e74c78f914b4fa1c91855e02b9fa1453eb46
zhanglujia <zhanglj@wisenjoy.com>
2016-11-26 18:45:15 +0800

    页面动态小图标

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -13,6 +13,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<link href="./favicon.ico" rel="shortcut icon">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
